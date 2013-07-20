4129bde90dd9b9e5d4d8b497898a52848fd15d01
Ivan Remen <bhyk@yandex.ru>
2013-07-21 02:02:03 +0400

    config singleton pattern is fixed

--- server/src/config.py
+++ server/src/config.py
@@ -11,6 +11,6 @@ class Config:
     _instance = None
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(
+            cls._instance = super(Config, cls).__new__(
                                  cls, *args, **kwargs)
         return cls._instance	
