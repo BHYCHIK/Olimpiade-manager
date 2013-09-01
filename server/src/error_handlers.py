067731734904a5c58f26be4109713e10eee2cbb8
Ivan Remen <bhyk@yandex.ru>
2013-09-01 22:39:25 +0400

    Bug fixed

--- server/src/error_handlers.py
+++ server/src/error_handlers.py
@@ -5,10 +5,16 @@ def request_not_valid_json():
     return '{"error_code": 1, "error_text": "Your request is not valid json"}'
 
 def unknown_api_function(request):
-    return '{"error_code": 2, id: %d, "error_text": "Unknown api function or no cmd field in request"}' % request["id"]
+    return '{"error_code": 2, "id": %d, "error_text": "Unknown api function or no cmd field in request"}' % int(request["id"])
 
 def no_id_parametr():
     return '{"error_code": 3, "error_text": "No id parametr"}'
 
 def not_enougth_args(request):
-    return '{"error_code": 4, "error_text": "No enough parametr", id: %d}' % request["id"]
+    return '{"error_code": 4, "error_text": "No enough parametr", "id": %d}' % int(request["id"])
+
+def sql_error(request):
+    return '{"error_code": 5, "error_text": "Sql error", "id": %d}' % int(request["id"])
+
+def unknown_error():
+    return '{"error_code": 999, "error_text": "Unknown error"}'
--- server/src/onp_api.py
+++ server/src/onp_api.py
@@ -13,43 +13,64 @@ def _check_args(request, *needed_args):
     return True
 
 def onp_ping(request):
-    return '{"error_code": 0, "id": %d}' % request["id"]
+    return '{"error_code": 0, "id": %d}' % int(request["id"])
 
 def onp_register_person(request):
     if not _check_args(request, "first_name", "second_name", "surname", "gender", "email", "date_of_birth", "description", "address", "phone"):
         return not_enougth_args(request)
     conf = config.Config()
-    conn = MySQLdb.connect(host=conf.db_host, user = conf.db_user, passwd = conf.db_pass, db = conf.db_name)
+    try:
+        conn = MySQLdb.connect(host=conf.db_host, user = conf.db_user, passwd = conf.db_pass, db = conf.db_name)
+    except Exception:
+        return sql_error(request)
     cur = conn.cursor()
 
     sql = "INSERT INTO person(first_name, second_name, surname, gender, email, date_of_birth, description, address, phone) VALUES(%(first_name)s, %(second_name)s, %(surname)s, %(gender)s, %(email)s, %(date_of_birth)s, %(description)s, %(address)s, %(phone)s)"
 
-    cur.execute(sql, request)
-    inserted_id = conn.insert_id()
-    conn.commit()
+    error_happend = False
+    try:
+        cur.execute(sql, request)
+        inserted_id = conn.insert_id()
+        conn.commit()
+    except Exception:
+         error_happend = True
+    finally:
+        cur.close()
+        conn.close()
+    if error_happend:
+        return sql_error(request)
 
-    return '{"error_code": 0, "id": %d, "person_id": %d}' % (request["id"], inserted_id)
-
-    cur.close()
-    conn.close()
+    return '{"error_code": 0, "id": %d, "person_id": %d}' % (int(request["id"]), inserted_id)
 
 def onp_register_account(request):
     if not _check_args(request, "login", "password", "person_id"):
         return not_enougth_args(request)
     conf = config.Config()
-    conn = MySQLdb.connect(host=conf.db_host, user = conf.db_user, passwd = conf.db_pass, db = conf.db_name)
+    try:
+        conn = MySQLdb.connect(host=conf.db_host, user = conf.db_user, passwd = conf.db_pass, db = conf.db_name)
+    except Exception:
+        return sql_error(request)
+
     cur = conn.cursor()
 
     sql = "INSERT INTO account VALUES(%(login)s, md5(%(password)s), %(person_id)d)"
 
-    cur.execute(sql, request)
-    conn.commit()
-    inserted_id = conn.insert_id()
+    error_happend = False
+    try:
+        cur.execute(sql, request)
+        conn.commit()
+        inserted_id = conn.insert_id()
 
-    return '{"error_code": 0, "id": %d, "account_id": %d}' % (request["id"], inserted_id)
+    except Exception:
+        error_happend = False
 
-    cur.close()
-    conn.close()
+    finally:
+        cur.close()
+        conn.close()
+    if error_happend:
+        return sql_error(request)
+
+    return '{"error_code": 0, "id": %d, "account_id": %d}' % (int(request["id"]), inserted_id)
 
 api_functions = {
     "onp_ping": onp_ping,
